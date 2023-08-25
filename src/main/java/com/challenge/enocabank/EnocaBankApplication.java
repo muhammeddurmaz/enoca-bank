a24fc07d5b1d23a93e17bdf8386129b2a2e87273
muhammeddurmaz <muhammed.durmaz@derinarge.com>
2023-08-25 17:40:47 +0300

    start

---
+++ src/main/java/com/challenge/enocabank/EnocaBankApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.enocabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnocaBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnocaBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/challenge/enocabank/EnocaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.enocabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnocaBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
