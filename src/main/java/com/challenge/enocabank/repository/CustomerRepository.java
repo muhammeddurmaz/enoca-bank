1a7a8f908df140bf465b6e020cfe6f33bc41d98f
muhammeddurmaz <muhammed.durmaz@derinarge.com>
2024-03-23 13:23:08 +0300

    .

--- src/main/java/com/challenge/enocabank/repository/CustomerRepository.java
+++ src/main/java/com/challenge/enocabank/repository/CustomerRepository.java
@@ -2,8 +2,14 @@ package com.challenge.enocabank.repository;
 
 import com.challenge.enocabank.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
+
+//    @Query("select * from customer left join account on account.customer_id = customer.id where account.id is null")
+//    List<Customer> findAllByNullAccount();
 }
